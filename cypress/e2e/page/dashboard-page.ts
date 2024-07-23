257d02d24bd8de19dceeb1484d74bc6c11adcba6
Gitsoumabha <soumabharoy47@gmail.com>
2024-07-23 00:40:51 -0400

    fix: selector change

--- cypress/e2e/page/dashboard-page.ts
+++ cypress/e2e/page/dashboard-page.ts
@@ -1,6 +1,6 @@
 export class DashboardPage {
 
-    private pageTitle = ".oxd-topbar-header-breadcrumb > .oxd-text"
+    private pageTitle = ".oxd-topbar-header-breadcrumb"
     public verifyDashboard(){
         cy.get(this.pageTitle).should(
             "have.text",
